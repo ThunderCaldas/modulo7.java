bb646bbbbb1836c5ff382957a6f878e8b80635a8
ThunderCaldas <103012178+ThunderCaldas@users.noreply.github.com>
2022-04-13 21:48:43 -0300

    Create ContaCorrente.java

---
+++ ContaCorrente.java
@@ -0,0 +1,42 @@
+public class ContaCorrente extends Conta {
+    private final double valorInicialChequeEspecial;
+    private double valorChequeEspecial;
+    private Math Match;
+
+    public ContaCorrente(double valorInicialChequeEspecial) {
+        this.valorInicialChequeEspecial = valorInicialChequeEspecial;
+    }
+
+    public double sacar(double valor) {
+        System.out.println("Sacando: " + valor);
+        if (valor <= (getSaldo() + valorChequeEspecial)) {
+            setSaldo(getSaldo() - valor);
+            System.out.println("Saldo atual: " + getSaldo());
+
+            if (getSaldo() < 0) {
+                System.out.println("Valor restante do cheque especial: " + getvalorChequeEspecial());
+            }
+            return valor;
+        } else {
+            System.out.println("Cheque especial indisponível para esse saque.");
+        }
+        return 0.0;
+    }
+
+    public double depositar(double valor) {
+        System.out.println("Depositando: " + valor);
+        setSaldo(getSaldo() + valor);
+        System.out.println("Saldo atual: " + getSaldo());
+
+        return valor;
+    }
+
+    private double getvalorChequeEspecial() {
+        double valorChequeEspecial = 100;
+        if (getSaldo() < 0)
+            return valorChequeEspecial - Match.abs(getSaldo());
+        else
+            return valorChequeEspecial;
+
+    }
+    }
