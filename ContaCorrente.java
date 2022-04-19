4e8591214e73485ad7a5b320fbf5bcafc31347d5
ThunderCaldas <103012178+ThunderCaldas@users.noreply.github.com>
2022-04-19 19:37:51 -0300

    Update ContaCorrente.java

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,9 +1,12 @@
-public class ContaCorrente extends Conta{
+public class ContaCorrente extends Conta implements  Tributos{
     private double chequeEspecial;
 
     public  ContaCorrente(int numero, int agencia, String banco, double saldo,double chequeEspecial) {
         super (numero, agencia, banco, saldo);
         this.chequeEspecial = chequeEspecial;
+    }
+    public double getChequeEspecial(){
+        return chequeEspecial;
     }
 
     @Override
@@ -12,19 +15,29 @@ public class ContaCorrente extends Conta{
         if(valor <= getSaldo()+chequeEspecial && getSaldo()+chequeEspecial - valor >0) {
             setSaldo(getSaldo() - valor);
             System.out.println("Operação concluída:");
-            System.out.println("Saldo restante com cheque especial:" + getSaldo());
+            System.out.println("Saldo restante com cheque especial:" + (getSaldo() + getChequeEspecial()));
         }else{
             System.out.println("Operação cancelada");
-            System.out.println("Saldo restante com cheque especial:" + getSaldo());
+            System.out.println("Saldo restante com cheque especial:" + (getSaldo() + getChequeEspecial()));
         }
-      return valor;
+        return getSaldo();
+    }
+
+
+    public double getTributos(){
+        double getTributos = (-10);
+        return getTributos;
     }
 
     @Override
     public double depositar(double valor) {
-        System.out.println("Depositando: " + valor);
+        System.out.println("Depositando: " + valor );
         setSaldo(getSaldo() + valor);
-        System.out.println("Seu saldo atualizado: " + getSaldo());
-        return valor;
+        System.out.println("Saldo total sem tributos: R$" + (getSaldo() + getTributos()));
+        System.out.println("Valor total com tributos: R$" + (getSaldo()));
+        System.out.println("Valor dos tributos: " + ( - getTributos()));
+        return getSaldo();
+
     }
+
 }
