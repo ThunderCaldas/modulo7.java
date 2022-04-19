69b7c972fd46c2a98fd114f7b34c2bcc1f3f0dc5
ThunderCaldas <103012178+ThunderCaldas@users.noreply.github.com>
2022-04-19 19:39:03 -0300

    Update ContaSalario.java

--- ContaSalario.java
+++ ContaSalario.java
@@ -1,10 +1,11 @@
-public class ContaSalario extends Conta{
+public class ContaSalario extends Conta implements Tributos{
     private double limiteSaque;
 
     public ContaSalario(int numero, int  agencia, String banco, double saldo, double limiteSaque){
         super (numero, agencia, banco, saldo);
         this.limiteSaque = limiteSaque;
     }
+
 
     @Override
     public double sacar(double valor) {
@@ -13,6 +14,8 @@ public class ContaSalario extends Conta{
         if (limiteSaque > 0) {
             setSaldo(getSaldo() - valor);
             limiteSaque--;
+            System.out.println("Operação concluída:");
+            System.out.println("Saldo restante:" + getSaldo());
             return valor;
         }else {
             System.out.println("Quantidade de saques ultrapassado");
@@ -22,13 +25,18 @@ public class ContaSalario extends Conta{
     }
 
         @Override
-    public double depositar(double valor) {
-        System.out.println("Depositando: " + valor );
-        setSaldo(getSaldo()+valor);
-        System.out.println("saldo atual: " + getSaldo());
-        return valor;
+        public double getTributos(){
+            double getTributos = (10);
+            return getTributos;
         }
 
-
-
+    @Override
+    public double depositar(double valor) {
+        System.out.println("Depositando: " + valor );
+        setSaldo(getSaldo() + valor);
+        System.out.println("Saldo total sem tributos: R$" + getSaldo());
+        System.out.println("Valor dos tributos: " + getTributos());
+        System.out.println("Valor total com tributos: R$" + (getSaldo() - getTributos()));
+        return valor;
+        }
 }
